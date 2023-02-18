a188f973406b0504f500058839c39f5347b825a6
John Irwin <jirwin@lightboxre.com>
2023-02-17 21:18:09 -0500

    reading files from directory

--- deploy/sync.js
+++ deploy/sync.js
@@ -19,6 +19,8 @@ function sync() {
     // console.log(apigeeUser);
     console.log("About to make directory");
     let contents = fs.readFileSync('./example.txt', 'utf-8')
+    var files = fs.readdirSync('/terraform/proxy/')
+    console.log(files);
     console.log(contents);
     const liveDeployments = JSON.parse(run(`apigeetool listdeployments ${apigeeCliCreds} -e ${apigeeEnvironment} -j`));
     const matches = liveDeployments.deployments.filter((cdict) => cdict.name === proxyName);
